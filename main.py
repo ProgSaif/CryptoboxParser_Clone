b8338ccedf163854f7eb8f2a2c87cfe11ca61ca6
ProgSaif <isaifulone@gmail.com>
2025-11-20 18:22:14 +0600

    Update main.py

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def parse_and_format_message(text):
     link = f'<a href="https://t.me/BinanceRedPacket_Hub">{emoji}</a>'
 
     # Format as HTML with monospace code
-    formatted = f"{link} <code>{html.escape(code)}</code>\n\n                𝑨𝒅𝒔        \n﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌ \n For #Square Follow \n @BinanceSquare_Hub \n #Binance #RedPacket"
+    formatted = f"{link} <code>{html.escape(code)}</code>\n\n                𝑨𝒅𝒔         \n For #Square Follow \n @BinanceSquare_Hub \n #Binance #RedPacket"
 
     return formatted
 
